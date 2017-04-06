13d5c8a60ae567f158d44fd398d9851b6482ba18
Igor Kuzmenko <igor@kuzzmi.com>
2017-04-06 17:10:17 +0200

    Added keyboard. Moving bacterrior

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -3,6 +3,9 @@ module Main exposing (..)
 import Html exposing (..)
 import Time exposing (Time)
 import AnimationFrame
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Keyboard
 
 
 main =
@@ -19,7 +22,17 @@ main =
 
 
 type alias Model =
-    {}
+    { bacterrior : Bacterrior
+    , shouldMoveTo : Position
+    }
+
+
+type Key
+    = KeyNone
+    | KeyArrowUp
+    | KeyArrowLeft
+    | KeyArrowRight
+    | KeyArrowDown
 
 
 type alias ID =
@@ -46,7 +59,15 @@ init =
 
 model : Model
 model =
-    {}
+    { bacterrior =
+        { id = "A"
+        , position = ( 0, 0 )
+        , healthPoints = 100
+        , level = 1
+        , experience = 0
+        }
+    , shouldMoveTo = ( 0, 0 )
+    }
 
 
 
@@ -56,6 +77,7 @@ model =
 type Msg
     = Noop
     | Tick Time
+    | KeyPress Key
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -65,21 +87,97 @@ update msg model =
             model ! []
 
         Tick time ->
-            model ! []
+            let
+                ( posX, posY ) =
+                    model.bacterrior.position
+
+                ( moveX, moveY ) =
+                    model.shouldMoveTo
+
+                bacterrior =
+                    model.bacterrior
+
+                update bacterrior =
+                    { bacterrior | position = ( posX + moveX, posY + moveY ) }
+            in
+                { model
+                    | bacterrior = update bacterrior
+                    , shouldMoveTo = ( 0, 0 )
+                }
+                    ! []
+
+        KeyPress key ->
+            case key of
+                KeyArrowUp ->
+                    { model | shouldMoveTo = ( 0, -1 ) } ! []
+
+                KeyArrowLeft ->
+                    { model | shouldMoveTo = ( -1, 0 ) } ! []
+
+                KeyArrowRight ->
+                    { model | shouldMoveTo = ( 1, 0 ) } ! []
+
+                KeyArrowDown ->
+                    { model | shouldMoveTo = ( 0, 1 ) } ! []
+
+                KeyNone ->
+                    model ! []
 
 
 
 -- SUBSCRIPTIONS
 
 
+subscriptions : Model -> Sub Msg
 subscriptions model =
-    AnimationFrame.times Tick
+    Sub.batch
+        [ AnimationFrame.times Tick
+        , Keyboard.downs keyPressed
+        ]
+
+
+keyPressed : Keyboard.KeyCode -> Msg
+keyPressed code =
+    case code of
+        37 ->
+            KeyPress KeyArrowLeft
+
+        38 ->
+            KeyPress KeyArrowUp
+
+        39 ->
+            KeyPress KeyArrowRight
+
+        40 ->
+            KeyPress KeyArrowDown
+
+        _ ->
+            KeyPress KeyNone
 
 
 
 -- VIEW
 
 
-view : Model -> Html msg
+bacterriorView : Bacterrior -> Html Msg
+bacterriorView bacterrior =
+    let
+        ( posX, posY ) =
+            bacterrior.position
+    in
+        rect
+            [ toString posX |> x
+            , toString posY |> y
+            , width "10"
+            , height "10"
+            , rx "1"
+            , ry "1"
+            ]
+            []
+
+
+view : Model -> Html Msg
 view model =
-    div [] [ text "Hello" ]
+    svg
+        [ width "120", height "120", viewBox "0 0 120 120" ]
+        [ bacterriorView model.bacterrior ]
